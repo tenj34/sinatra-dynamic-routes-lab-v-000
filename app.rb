78b4b5145cd35f121f01dc97c6612e467d1298e4
Juan Tenezaca <tenj34@gmail.com>
2018-10-18 02:41:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name' do 
-
+  get '/reversename/:name' do
+    @name = params[:name]
   end
 end
