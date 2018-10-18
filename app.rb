abf9e206e93b6ac4dd31a81e774f58a1a9cd5113
Juan Tenezaca <tenj34@gmail.com>
2018-10-18 02:50:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,12 +23,29 @@ class App < Sinatra::Base
 
     "#{@combined_phrase}"
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @all_words = []
     params.each {|key, word| @all_words << word}
     "#{@all_words.join(" ")}."
   end
+
+  get '/:operation/:number1/:number2' do
+  @operation = params[:operation]
+  @number1 = params[:number1].to_i
+  @number2 = params[:number2].to_i
+   case @operation
+  when "add"
+    @num = @number1 + @number2
+  when "subtract"
+    @num = @number1 - @number2
+  when "divide"
+    @num = @number1 / @number2
+  when "multiply"
+    @num = @number1 * @number2
+  end
+  "#{@num}"
+end
 
 
 end
